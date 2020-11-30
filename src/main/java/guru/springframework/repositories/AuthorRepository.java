33fae7c35634e772c60984b14d50c8df9912a3cf
antocecere <antocecere77@gmail.com>
2020-11-30 06:53:12 +0100

    Trigger build on Jenkins

--- src/main/java/guru/springframework/repositories/AuthorRepository.java
+++ src/main/java/guru/springframework/repositories/AuthorRepository.java
@@ -3,8 +3,5 @@ package guru.springframework.repositories;
 import guru.springframework.domain.Author;
 import org.springframework.data.repository.CrudRepository;
 
-/**
- * Created by jt on 5/6/16.
- */
 public interface AuthorRepository extends CrudRepository<Author, Integer> {
 }
