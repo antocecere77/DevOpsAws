c73f6a38ae72fa33de4ffaf275d51dae46db1343
antocecere <antocecere77@gmail.com>
2020-11-30 07:04:25 +0100

    Another trigger build on Jenkins

--- src/main/java/guru/springframework/repositories/ProductCategoryRepository.java
+++ src/main/java/guru/springframework/repositories/ProductCategoryRepository.java
@@ -3,8 +3,5 @@ package guru.springframework.repositories;
 import guru.springframework.domain.ProductCategory;
 import org.springframework.data.repository.CrudRepository;
 
-/**
- * Created by jt on 5/6/16.
- */
 public interface ProductCategoryRepository extends CrudRepository<ProductCategory, Integer> {
 }
